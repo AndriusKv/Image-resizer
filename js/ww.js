6190a60172088ce6be71939d1a37ffe428d03de9
Andrius Kvedaras <ikandrius@gmail.com>
2016-11-29 14:28:05 +0200

    Update with changes made on master branch

--- js/ww.js
+++ js/ww.js
@@ -2,13 +2,13 @@
 
 /* global importScripts, JSZip, onmessage, postMessage */
 
-importScripts("../libs/jszip.min.js");
+importScripts("../js/libs/jszip.min.js");
 
 var zip = new JSZip();
 var content = null;
 var i = 0;
 
-function zipImages(name, uri, type) {
+function addToFolder(name, uri, type) {
     zip.folder("images").file(name + "." + type, uri, { base64: true });
 }
 
@@ -23,17 +23,24 @@ function truncateUri(uri, type) {
     return uri.slice(22);
 }
 
+function zipImage(image, i) {
+    var type = changeFileType(image.type);
+    var uri = truncateUri(image.uri, type);
+    var name = image.name + i;
+
+    addToFolder(name, uri, type);
+}
+
 onmessage = function onmessage(event) {
     var data = event.data;
 
     switch (data.action) {
         case "add":
-            var image = data.image;
-            var type = changeFileType(image.type);
-            var uri = truncateUri(image.uri, type);
-
-            zipImages(image.name + i, uri, type);
+            zipImage(data.image, i);
             i += 1;
+            break;
+        case "add-bulk":
+            data.images.forEach(zipImage);
             break;
         case "generate":
             i = 0;
