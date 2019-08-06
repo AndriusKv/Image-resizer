d1e24cd65a070897765fcfdd11f16c1dc21192c0
Travis CI <ikandrius@gmail.com>
2019-08-06 15:34:04 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.bd78c014a0eb344533bf3f636d88f29d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e288101cadbc3c795f1acc929c0c55",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "b2527a0d46b2a08af352b3f92e2a67a3",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "revision": "e3ff22abda2aafb4889a8321e825bb47",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "b45c2be3d5201f7083be070124ae3436",
+    "url": "assets/images/logo.png"
+  },
+  {
+    "revision": "5eb948023100bf6eb33094b3d68099eb",
+    "url": "assets/images/pattern.png"
+  },
+  {
+    "revision": "31aaf6d4b6a55800092bace6e8f333ee",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "66ee68f14bdc11359dec7280cc64cf12",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "b6ba326cd1a65d02fc6f0887fb0961c1",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "6b960fa84c5a104d84d8ef6748223835",
+    "url": "index.html"
+  },
+  {
+    "revision": "dc5d2aac976b1ad09faa452b4ce37519",
+    "url": "libs/jszip.min.js"
+  },
+  {
+    "revision": "162edd103c19a2285ceb",
+    "url": "main.css"
+  },
+  {
+    "revision": "162edd103c19a2285ceb",
+    "url": "main.js"
+  },
+  {
+    "revision": "e3205ad2854d100157e9de066a5bd892",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "01c55aed941321174829",
+    "url": "vendor.js"
+  },
+  {
+    "revision": "be65f943843266ea7aceddc8c06281ab",
+    "url": "ww.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,24 @@
+importScripts("precache-manifest.bd78c014a0eb344533bf3f636d88f29d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* global workbox */
+
+workbox.setConfig({
+  debug: false
+});
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+self.addEventListener("install", event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
