7404ff782380d95e4cbe3fb48a19ecae582dc64e
andriuskv <ikandrius@gmail.com>
2023-10-08 13:42:52 +0000

    Deploying to gh-pages from @ andriuskv/imagis@62e05c8ae1754efe97bae9d3dde13df7409d6c70 🚀

--- ww.js
+++
@@ -1,1 +0,0 @@
-importScripts("./libs/jszip.min.js");const zip=new JSZip;self.onmessage=async function(e){zip.remove("images"),e.data.forEach((e=>{zip.folder("images").file(e.name,e.file)})),postMessage(await zip.generateAsync({type:"blob"}))};
